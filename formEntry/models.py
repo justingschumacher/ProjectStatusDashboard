1b8544d0fa0efe13bf88c17c334f845ff725364a
Justin Schumacher <justingschumacher@icloud.com>
2017-11-24 16:22:04 -0800

    pep8 fixes

--- formEntry/models.py
+++ formEntry/models.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from simple_history.models import HistoricalRecords
+# from simple_history.models import HistoricalRecords
 
 
 # Create your models here.
@@ -40,6 +40,7 @@ class Project(models.Model):
 
     def __str__(self):
         return self.goal
+
 
 class ProjectStatusIndicator(models.Model):
     projectStatusIndicator = models.BooleanField('Project Status: ')
--- formEntry/urls.py
+++ formEntry/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^project/new/$', views.project_new, name='project_new'),
-    #url(r'^project/detail/$', views.detail, name='project_detail'),
     url(r'^project/detail/(?P<pk>\d+)$', views.ProjectEdit.as_view(), name='project_edit'),
-
 ]
