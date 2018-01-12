550f1bd5a2879ee541f9154d572431c277674e19
Ubuntu <ubuntu@ip-172-28-5-83.us-west-2.compute.internal>
2018-01-12 23:13:53 +0000

    added gunicorn to git ignore

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "127.0.0.1:8000"                   # Don't use port 80 becaue nginx occupied it already. 
-errorlog = '/Users/justin/logs/gunicorn-projectstatusdashboard-error.log'  
-accesslog = '/Users/justin/logs/gunicorn-projectstatusdashboard-access.log'
+errorlog = '/home/ubuntu/logs/gunicorn-goaldashboard-error.log'
+accesslog = '/home/ubuntu/logs/gunicorn-goaldashboard-access.log'
 loglevel = 'debug'
-workers = 1     # the number of recommended workers is '2 * number of CPUs + 1' 
+workers = 3     # the number of recommended workers is '2 * number of CPUs + 1' 
