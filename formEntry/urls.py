9536fea6a9d744fcb201394092270a0b353e63ca
Justin Schumacher <justin.schumacher@nordstrom.com>
2018-01-17 14:03:47 -0800

    fix bug with rev. path and add filter to main view

--- formEntry/urls.py
+++ formEntry/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import url
-from django.urls import path,re_path
+from django.urls import path, re_path
 from . import views
 
-
+# app_name = 'formEntry'
 urlpatterns = [
-    url('^$', views.index, name='project_index'),
+    path('', views.index, name='project_index'),
     # path(r'', views.ProjectIndexView.as_view(), name='project_index'),
     path('project/new/', views.ProjectNewView.as_view(), name='project_new'),
     re_path(r'^project/detail/(?P<pk>\d+)$', views.ProjectDetailView.as_view(), name='project_detail'),
     re_path(r'^project/update/(?P<pk>\d+)$', views.ProjectUpdateView.as_view(), name='project_update'),
-    # url(r'^project/update/(\d+)$', views.projectupdateview, name='project_update'),
     re_path(r'^project/delete/(?P<pk>\d+)$', views.ProjectDeleteView.as_view(), name='project_delete'),
 ]
--- formEntry/views.py
+++ formEntry/views.py
@@ -1,56 +1,57 @@
 from django.db.models import Q
-from django.views.generic.edit import CreateView, UpdateView, View, FormView
-from django.views.generic import ListView, DeleteView, DetailView
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect, render_to_response
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DeleteView, DetailView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.urls import reverse_lazy
-from django.http import HttpResponse
-from django.contrib import messages
 from .models import Project
 from .forms import ProjectForm
 
 
 # Create your views here.
 
-class ProjectIndexView(DetailView):
-
-    model = Project
-
-    fields = ('projectRecordID', 'goal', 'owner', 'group', 'restrictedStatus', 'startDate', 'dueDate', 'revisedDueDate',
-              'completionDate', 'didNotMeetDate', 'projectStatus', 'linkToMetrics', 'deck', 'name',
-              'description', 'comments', 'executiveSummary', 'definition', 'createdDate', 'editDate',
-              'pathToGreen', 'previousMilestone', 'currentMilestone', 'inputGoals', 'outputGoals',
-              'goalType', 'projectCompletionStatus')
-
-    template_name = 'formEntry/project_index.html'
-
-    success_url = reverse_lazy('project_index')
-
-    def get(self, request, *args, **kwargs):
-        projects = Project.objects.order_by('dueDate')
-        searchterm = ''
-        filterterm = ''
-        if request.POST and request.POST.get('search'):
-            searchterm = request.POST.get('search').lower()
-            projects = projects.filter(Q(group__icontains=searchterm) |
-                                       Q(name__icontains=searchterm) |
-                                       Q(projectStatus__icontains=searchterm) |
-                                       Q(goal__icontains=searchterm) |
-                                       Q(owner__icontains=searchterm)
-                                       )
-        if request.POST and request.POST.get('filter'):
-            filterterm = request.POST.get('filter').lower()
-            projects = projects.filter(Q(group__icontains=filterterm) |
-                                       Q(name__icontains=filterterm) |
-                                       Q(projectStatus__icontains=filterterm) |
-                                       Q(goal__icontains=filterterm) |
-                                       Q(owner__icontains=filterterm)
-                                       )
-        return render(request, 'formEntry/project_index.html', {'Projects': projects, 'searchterm': searchterm})
+# class ProjectIndexView(DetailView):
+#
+#     model = Project
+#
+#     fields = ('projectRecordID', 'goal', 'owner', 'group', 'restrictedStatus', 'startDate', 'dueDate', 'revisedDueDate',
+#               'completionDate', 'didNotMeetDate', 'projectStatus', 'linkToMetrics', 'deck', 'name',
+#               'description', 'comments', 'executiveSummary', 'definition', 'createdDate', 'editDate',
+#               'pathToGreen', 'previousMilestone', 'currentMilestone', 'inputGoals', 'outputGoals',
+#               'goalType', 'projectCompletionStatus')
+#
+#     template_name = 'formEntry/project_index.html'
+#
+#     success_url = reverse_lazy('project_index')
+#
+#     def get(self, request, *args, **kwargs):
+#         projects = Project.objects.order_by('dueDate')
+#         searchterm = ''
+#         filterterm = ''
+#         if request.POST and request.POST.get('search'):
+#             searchterm = request.POST.get('search').lower()
+#             projects = projects.filter(Q(group__icontains=searchterm) |
+#                                        Q(name__icontains=searchterm) |
+#                                        Q(projectStatus__icontains=searchterm) |
+#                                        Q(goal__icontains=searchterm) |
+#                                        Q(owner__icontains=searchterm) |
+#                                        Q(projectCompletionStatus__icontains=searchterm)
+#                                        )
+#         if request.POST and request.POST.get('filter'):
+#             filterterm = request.POST.get('filter').lower()
+#             projects = projects.filter(Q(group__icontains=filterterm) |
+#                                        Q(name__icontains=filterterm) |
+#                                        Q(projectStatus__icontains=filterterm) |
+#                                        Q(goal__icontains=filterterm) |
+#                                        Q(owner__icontains=filterterm) |
+#                                        Q(projectCompletionStatus__icontains=searchterm)
+#                                        )
+#         return render(request, 'formEntry/project_index.html', {'Projects': projects, 'searchterm': searchterm})
 
 
 def index(request):
-    projects = Project.objects.order_by('dueDate')
+    projects = Project.objects.exclude(projectCompletionStatus__startswith="Completed"
+                                       ).order_by('dueDate')
     searchterm = ''
     filterterm = ''
     if request.POST and request.POST.get('search'):
@@ -59,7 +60,8 @@ def index(request):
                                    Q(name__icontains=searchterm) |
                                    Q(projectStatus__icontains=searchterm) |
                                    Q(goal__icontains=searchterm) |
-                                   Q(owner__icontains=searchterm)
+                                   Q(owner__icontains=searchterm) |
+                                   Q(projectCompletionStatus__icontains=searchterm)
                                    )
     if request.POST and request.POST.get('filter'):
         filterterm = request.POST.get('filter').lower()
@@ -67,27 +69,13 @@ def index(request):
                                    Q(name__icontains=filterterm) |
                                    Q(projectStatus__icontains=filterterm) |
                                    Q(goal__icontains=filterterm) |
-                                   Q(owner__icontains=filterterm)
+                                   Q(owner__icontains=filterterm) |
+                                   Q(projectCompletionStatus__icontains=searchterm)
                                    )
     return render(request,
                   'formEntry/project_index.html',
                   {'Projects': projects,
                    'searchterm': searchterm})
-
-
-# def projectupdateview(request):
-#     form = ProjectForm(request.POST or None)
-#
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             form.save()
-#             return redirect('project_index')
-#         else:
-#             return HttpResponse('Error!')
-#
-#     context = {'form': form}
-#
-#     return render(request, 'formEntry/project_update.html', context)
 
 
 class ProjectNewView(CreateView):
@@ -102,12 +90,9 @@ class ProjectNewView(CreateView):
 
     template_name = 'formEntry/project_new.html'
 
-    success_url = reverse_lazy('formEntry:project_index')
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProjectNewView, self).get_context_data(**kwargs)
-    #     context['now'] = timezone.now()
-    #     return context
+    success_url = reverse_lazy('project_index')
+
+
 
 
 class ProjectUpdateView(UpdateView):
