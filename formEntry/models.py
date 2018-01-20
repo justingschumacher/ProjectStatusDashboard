836bf6c29f274f99a8d6a65f87e75d6d1a05e0f4
Justin Schumacher <justin.schumacher@nordstrom.com>
2018-01-19 16:07:47 -0800

    creating model form

--- formEntry/models.py
+++ formEntry/models.py
@@ -79,3 +79,9 @@ class Project(models.Model):
 
     def __str__(self):
         return self.goal
+
+class ProjectForm(ModelForm):
+    class meta:
+        model = Project
+        fields = '__all__'
+        
